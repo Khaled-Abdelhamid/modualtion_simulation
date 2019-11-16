b84d5f4196f571a5d7dfd79b2a53e88202ba7f00
Khaled Abdelhamid <khaledusama1998@gmail.com>
2019-11-16 19:31:11 +0200

    QPSK

--- QPSK.m
+++ QPSK.m
@@ -2,7 +2,7 @@ clear
 clc
 close all
 
-bitsNo=3000;% the
+bitsNo=5;% the
 info=randi([0,1],1,bitsNo);
 
 Ts=.01;% smapling rate
@@ -12,26 +12,32 @@ fc=Nc/Tb;
 
 bit=0:Ts:Tb-Ts;%simulation of each bit
 
-Eb=4;%energy per bit (those numbers are chosen to make calculations easier)
+Eb=8;%energy per bit (those numbers are chosen to make calculations easier)
 Ab=sqrt(Eb);%bit Amplitude
 
 mean =0;
-N0=0;
+N0=10;
 var=N0/2;
 
-basis=sqrt(2/Tb)*cos(2*pi*fc*bit);
+basis1=sqrt(2/Tb)*cos(2*pi*fc*bit);
+basis2=sqrt(2/Tb)*sin(2*pi*fc*bit);
+
+even=info(2:2:end);
+odd=info(1:2:end);
+
 Tx=[];
-for i=1:bitsNo %the loop of the sending
-    res=(-1)^(info(i)+1) * Ab *basis;
-    Tx=[Tx res];
+for i=1:bitsNo/2 %the loop of the sending
+    inPhase=(-1)^(even(i)+1) * Ab *basis1;
+    quadPhase=(-1)^(odd(i)+1) * Ab *basis2;
+    Tx=[Tx inPhase+quadPhase];
 end
 
 AWGNnoise=sqrt(var)*rand(1,length(Tx))+mean;%adding AWGN to the channel
 Txnoise=Tx+AWGNnoise;
 
-% plot(Tx)
-% figure
-% plot(Txnoise)
+plot(Tx)
+figure
+plot(Txnoise)
 
 %*******************************************
 % reciever
